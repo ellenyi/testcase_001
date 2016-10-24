8de9a6677e55203aff65d51ca3ce9a4dc04034fd
ellenyi <hongyan2007@163.com>
2016-10-25 01:08:18 +0800

    delete 1

--- openurl.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- encoding:UTF-8 -*-
-import webbrowser
-url = 'http://172.20.99.166/'
-#webbrowser.open(url)
-webbrowser.open(url,new=0,autoraise=True)
-print webbrowser.get()
