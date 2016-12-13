7e26fd41ad04ceeb0259e2271535f5595cc0a2d9
ellenyi <hongyan2007@163.com>
2016-12-13 17:33:28 +0800

    cleanenv

--- SourceCode/Python_Dir/Commskykeyword/Userdefined/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Various generic utility functions and classes, mainly for internal usage
-"""
- 
-from .converters import *
-from .get_path import *
-from .func_property import *
-from .keywordclass import *
- 
-
